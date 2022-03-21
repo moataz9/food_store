2130ead839b2af243950b0c90917bc582f2e4f9f
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-03-21 22:29:06 +0200

    first js files

---
+++ js/app.js
@@ -0,0 +1,18 @@
+import mainPage from './main-page-data.js'
+const main_categories = document.getElementById('main_categories')
+
+for (const item of mainPage) {
+  console.log(item);
+  main_categories.innerHTML += /* html */ `
+  <div class="col">
+    <a href="${item.goTo}" class="text-decoration-none">
+      <div class="text-center">
+        <div class="image-holder">
+          <img class="image" src="${item.imageSrc}" alt="${item.title}" />
+        </div>
+        <h2 class="font-weight-bold text-capitalize">${item.title}</h2>
+      </div>
+    </a>
+  </div>
+  `
+}
---
+++ js/main-page-data.js
@@ -0,0 +1,32 @@
+export default [
+  // {
+  //   title: 'fish',
+  //   imageSrc: './images/main/fish.jpg',
+  //   goTo: 'pages/fish.html',
+  // },
+  {
+    title: 'fruits',
+    imageSrc: './images/main/fruits.jpg',
+    goTo: 'pages/fruits.html',
+  },
+  // {
+  //   title: 'grocery',
+  //   imageSrc: './images/main/grocery.jpg',
+  //   goTo: 'pages/grocery.html',
+  // },
+  // {
+  //   title: 'meat',
+  //   imageSrc: './images/main/meat.jpg',
+  //   goTo: 'pages/meat.html',
+  // },
+  // {
+  //   title: 'spices',
+  //   imageSrc: './images/main/spices.jpg',
+  //   goTo: 'pages/spices.html',
+  // },
+  {
+    title: 'vegetables',
+    imageSrc: './images/main/vegetables.jpg',
+    goTo: 'pages/vegetables.html',
+  },
+]
